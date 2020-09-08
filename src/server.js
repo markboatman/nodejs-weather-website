afec1c44ef8e2a1316e221bf72f9624f1c76f130
Mark Boatman <markjboat@yahoo.com>
2020-09-08 19:22:45 +0800

    server v1.2

--- src/server.js
+++ src/server.js
@@ -1,43 +1,51 @@
-// version features: added some more urls and responses
-const express = require('express');
+// version features:  
+// installed npm -i hbs, handbars module that extends express functionality
+// telling app what the view engine is, i.e. handlebars
+// telling the app where the template dir is for hbs templates is
 
-console.log(__dirname);
-console.log(__filename);
+// core modules
+const path = require('path');
+// npm modules
+const express = require('express');
 
 // create an express app, or server
 const app = express();
 
-// define what the app should do when someone sends a request
-// to a specific url
-// app.com
-// app.com/help
-// app.com/about
+// define paths for express config
+const veiwsPath = path.join(__dirname, '../templates');
+const publicPath = path.join(__dirname, '../public');
+//
+// set up integration of hbs with our express server
+app.set('view engine', 'hbs');
+// tell app where the views dir is
+app.set('views', veiwsPath);
+// set up static directory for other for static html
+app.use(express.static(publicPath));
 
 // this listener for url = app.com, req is reqest, res is response
 app.get('', (req, res) => {
-    // send html
-    res.send('<h1>Weather</h1>');
+    // this is a hbs call under the hood of express
+    res.render('index',{
+        title: 'Weather App',
+        name: 'Mark'
+    });
 })
 
-// this listener for url = app.com/help
-app.get('/help', (req, res) => {
-    // send json, express will stringify it for us
-    res.send([{
-        name: 'Mark',
-        age:  '64'
-    },
-    {
-        name:   'Fred',
-        age:    '34'
-    }]);
-})
-
-// this listener for url = app.com/about
 app.get('/about', (req, res) => {
-    // send back to requester
-    res.send('<h2>About Page</h2>');
+    // this is a hbs call under the hood of express
+    res.render('about',{
+        title: 'This is the hbs about page',
+        name: 'Mark'
+    });
 })
 
+app.get('/help', (req, res) => {
+    // this is a hbs call under the hood of express
+    res.render('help',{
+        title: 'This is the hbs help page',
+        name: 'Mark'
+    });
+})
 // this listener for url = app.com/weather
 app.get('/weather', (req, res) => {
     // send back to requester
